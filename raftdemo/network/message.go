feecfadd27ec9167c2f1059592fe33e61383870c
gdr-00 <gabriele.dare00@gmail.com>
2025-04-29 13:11:43 +0200

    refactor message

--- raftdemo/network/message.go
+++ raftdemo/network/message.go
@@ -1,1 +1,10 @@
 package network
+
+type MessageType int
+
+type Message struct {
+	From    string
+	To      string
+	Type    MessageType
+	Payload interface{}
+}
--- raftdemo/network/transport.go
+++ raftdemo/network/transport.go
@@ -1,18 +1,9 @@
 package network
 
-type MessageType int
-
 const (
 	MsgRequestVote MessageType = iota
 	MsgAppendEntries
 )
-
-type Message struct {
-	From    string
-	To      string
-	Type    MessageType
-	Payload interface{}
-}
 
 type Transport interface {
 	Send(msg Message) error
